dbe0fd7e76c0776de8504221a15100600b0f0ccb
sikirha <sikirha@gmail.com>
2023-04-13 23:26:04 +0900

    login logout

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,22 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { FiShoppingBag } from "react-icons/fi";
 import { BsFillPencilFill } from "react-icons/bs";
+import { login, logout, onUserStateChange } from "../api/firebase";
 
 export default function NavBar() {
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    onUserStateChange(setUser);
+  }, []);
+
   return (
-    <header>
-      <Link to="/">
+    <header className="flex p-2 font-semibold justify-between border-b border-gray-300">
+      <Link to="/" className="flex items-center text-4xl text-brand">
         <FiShoppingBag />
-        <h2>Shoppy</h2>
+        <h2 className="text-3xl">Shoppy</h2>
       </Link>
-      <nav>
+      <nav className="flex items-center gap-4">
         <Link to="/products">Products</Link>
         <Link to="/carts">Carts</Link>
-        <Link to="/products/new">
+        <Link to="/products/new" className="text-2xl">
           <BsFillPencilFill />
         </Link>
-        <button>Login</button>
+        {!user && <button onClick={login}>Login</button>}
+        {user && <button onClick={logout}>Logout</button>}
       </nav>
     </header>
   );
